22c738602fd4b5524e2b3bcaeb40e7e4e9894698
Francesco Esposito <33671357+frab1t@users.noreply.github.com>
2019-03-21 15:02:33 +0100

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import Timer from './components/Timer';
+import ButtonBox from './components/ButtonBox';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <div className="App-header title">
-        tomadoro
-      </div>
-       <Timer />
-       <div className="footer">
-        <p>
-          <a href="https://github.com/frab1t/tomadoro">tomadoro</a>{` `}
-          by <a href="https://twitter.com/frab1t">@frab1t</a> (Francesco Esposito)
-        </p>
-      </div>
+        <div
+          className="App-header title"
+        >
+          tomadoro
+        </div>
+        
+        <div>
+          <Timer />
+        </div>
+
+        <div
+          className="footer"
+        >
+          <p>
+            <a href="https://github.com/frab1t/tomadoro">tomadoro</a>{` `}
+            by <a href="https://twitter.com/frab1t">@frab1t</a> (Francesco Esposito)
+          </p>
+        </div>
       </div>
     );
   }
