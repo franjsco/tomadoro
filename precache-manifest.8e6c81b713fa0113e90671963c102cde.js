476adf797ce9f3301040366bf26d2d9b35b32fab
Francesco Esposito <33671357+frab1t@users.noreply.github.com>
2019-04-11 11:12:19 +0200

    Updates

---
+++ precache-manifest.8e6c81b713fa0113e90671963c102cde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec7925575d8f686a3e0aa503b82e7dd7",
+    "url": "/tomadoro/static/media/sound.ec792557.mp3"
+  },
+  {
+    "revision": "08ea95087788a7669d1e3f26e93b73fb",
+    "url": "/tomadoro/static/media/logo.08ea9508.svg"
+  },
+  {
+    "revision": "9dd617b37879fc98c25c",
+    "url": "/tomadoro/static/js/runtime~main.c4673ac0.js"
+  },
+  {
+    "revision": "fdf76d6f6f075290a219",
+    "url": "/tomadoro/static/js/main.38d995b8.chunk.js"
+  },
+  {
+    "revision": "1833adb1901133b31eae",
+    "url": "/tomadoro/static/js/2.6c737709.chunk.js"
+  },
+  {
+    "revision": "fdf76d6f6f075290a219",
+    "url": "/tomadoro/static/css/main.d0aea8b6.chunk.css"
+  },
+  {
+    "revision": "1833adb1901133b31eae",
+    "url": "/tomadoro/static/css/2.e75e5890.chunk.css"
+  },
+  {
+    "revision": "9b069a5398d4ffcd79dddda411f461e2",
+    "url": "/tomadoro/index.html"
+  }
+];
