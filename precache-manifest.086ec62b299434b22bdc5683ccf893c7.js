ba03d993821056bfecee0e7a8e18d3b0a111be45
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-10-24 17:21:42 +0200

    Updates

---
+++ precache-manifest.086ec62b299434b22bdc5683ccf893c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb79a6df92e8ee0dc9b3e420357cd7a9",
+    "url": "/tomadoro/index.html"
+  },
+  {
+    "revision": "3aa35ac144e96857120e",
+    "url": "/tomadoro/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "41a29cdb3aad9fbd9b87",
+    "url": "/tomadoro/static/css/main.b5bb2103.chunk.css"
+  },
+  {
+    "revision": "3aa35ac144e96857120e",
+    "url": "/tomadoro/static/js/2.dfd5301c.chunk.js"
+  },
+  {
+    "revision": "41a29cdb3aad9fbd9b87",
+    "url": "/tomadoro/static/js/main.257e49c2.chunk.js"
+  },
+  {
+    "revision": "055bcec9d9fcacfc57ce",
+    "url": "/tomadoro/static/js/runtime-main.6dad65da.js"
+  },
+  {
+    "revision": "08ea95087788a7669d1e3f26e93b73fb",
+    "url": "/tomadoro/static/media/logo.08ea9508.svg"
+  },
+  {
+    "revision": "ec7925575d8f686a3e0aa503b82e7dd7",
+    "url": "/tomadoro/static/media/sound.ec792557.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tomadoro/precache-manifest.5824c1bbb2f6638447f83f37572f2945.js"
+  "/tomadoro/precache-manifest.086ec62b299434b22bdc5683ccf893c7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tomadoro/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tomadoro/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
